4a79746426a36265dae7b3772d3fb0453b9f9c7f
Nicolas Milcoff <nicolasmilcoff@gmail.com>
2020-03-01 17:25:25 -0300

    Added sample TestApp

---
+++ TestApp/TestApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TestApp.Services;
+using TestApp.Views;
+
+namespace TestApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
