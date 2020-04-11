f07516272c20f0ff617f276cc5c2187f2ec3a20e
Nico Milcoff <nicolasmilcoff@gmail.com>
2020-04-11 17:38:02 -0300

    Showcase VersionTracker to ask users to update the app

--- TestApp/TestApp/App.xaml.cs
+++ TestApp/TestApp/App.xaml.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 using Xamarin.Forms; 
-using TestApp.Views;
+using TestApp.Views; 
 
 namespace TestApp
 {
     public partial class App : Application
-    {
-
+    { 
         public App()
         {
             InitializeComponent();
-
-            MainPage = new ChecksPage();
         }
 
         protected override void OnStart()
         {
+            // if we want to force users to update to a new version, we can send a silent push notification
+            // with the version that is no longer supported, and then compare that here
+            var deprecatedVersion = Xamarin.Essentials.Preferences.Get("DeprecatedVersion", string.Empty);
+            if (Xamarin.Essentials.VersionTracking.CurrentVersion == deprecatedVersion)
+            {
+                MainPage = new PleaseUpdateAppPage();
+            }
+            else
+            {
+                MainPage = new ChecksPage();
+            }
         }
 
         protected override void OnSleep()
